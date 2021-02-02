bebecd4650dbb82e2871f26053f7e7c0bccd1646
Mallika Goel <46377754+mallikagoel1602@users.noreply.github.com>
2021-02-02 21:27:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ def predict():
     output = round(prediction[0],2)
     return render_template('index.html',prediction_text="Number of Weekly Rides Should be {}".format(math.floor(output)))
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
